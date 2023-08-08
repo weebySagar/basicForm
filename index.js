dc65af70950193db72e0b27d96ba78c0d2bb70cd
Sagar <sagarmaurya814@gmail>
2023-08-08 23:17:08 +0530

    get-elements-by-classname

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const header = document.querySelector("#main-header");
 const title = document.querySelector(".title");
+const lists = document.getElementsByClassName("list-group-item");
+
 
 title.style.color="green";
 title.style.fontWeight="bold"
-header.style.borderBottom ="5px solid black"
+header.style.borderBottom ="5px solid black";
+
+// get elements by classname assignment
+lists[2].style.backgroundColor="green";
+for(let list of lists){
+    list.style.fontWeight= "bold"
+}
