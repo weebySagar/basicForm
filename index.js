73d2b029fc6d1fbf8e9a97ee4bfe8a96ffc7b310
Sagar <sagarmaurya814@gmail>
2023-08-08 22:09:59 +0530

    let's get started with dom

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const header = document.querySelector("#main-header");
+const title = document.querySelector(".title");
+
+title.style.color="green";
+title.style.fontWeight="bold"
+header.style.borderBottom ="5px solid black"
