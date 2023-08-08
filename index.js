8771dafc396393786adf69eed55fdc602473f8b7
Sagar <sagarmaurya814@gmail>
2023-08-08 23:26:19 +0530

    get-elements-by-tagname

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("#main-header");
 const title = document.querySelector(".title");
 const lists = document.getElementsByClassName("list-group-item");
+const listEl = document.getElementsByTagName("li");
+console.log(listEl);
 
 
 title.style.color="green";
@@ -11,4 +13,14 @@ header.style.borderBottom ="5px solid black";
 lists[2].style.backgroundColor="green";
 for(let list of lists){
     list.style.fontWeight= "bold"
+}
+
+
+// get elements by tag name assignment
+for(let list of lists){
+    list.style.backgroundColor="#ccc"
+}
+
+for(let list of listEl){
+    list.style.backgroundColor ="skyblue"
 }
