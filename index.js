89b4ff1a9870e1d90916c28b64248968d8badf0d
Sagar <sagarmaurya814@gmail>
2023-08-09 14:19:57 +0530

    filter

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const form = document.getElementById('addForm');
 const items = document.getElementById('items');
+const filter = document.getElementById('filter');
 
 form.addEventListener('submit',addItem);
 items.addEventListener('click',deleteItem);
+filter.addEventListener('keyup',filterItem)
 
 
 // Add item to the list
@@ -10,9 +12,12 @@ items.addEventListener('click',deleteItem);
 function addItem(e){
     e.preventDefault();
     const item = document.getElementById('item').value;
+    const description = document.getElementById('description').value;
     const li = document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(item));
+    li.appendChild(document.createTextNode(`  ${description}`));
+
 
     // delete button
     const deleteBtn = document.createElement('button');
@@ -28,16 +33,34 @@ function addItem(e){
 
 
     items.appendChild(li);
-    document.getElementById('item').value=""
+    document.getElementById('item').value="";
+    document.getElementById('description').value=""
 }
 
 
-// Delete item form the list
-
-
+// Delete item from the list
 function deleteItem(e){
     if(e.target.classList.contains('delete')){
         const li = e.target.parentElement;
         items.removeChild(li)
     }
+}
+
+
+// filter item from the list
+function filterItem(e){
+    const searchedItem = e.target.value.toLowerCase();
+
+    const items = document.getElementsByTagName('li');
+    Array.from(items).forEach((item)=>{
+        
+        const itemName= item.firstChild.textContent.toLowerCase() ;
+        const desription = item.childNodes[1].textContent.toLowerCase()
+        if(itemName.indexOf(searchedItem)!=-1 || desription.indexOf(searchedItem)!=-1){
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
 }
