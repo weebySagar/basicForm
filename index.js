9ee7351d9714abd196385ab22fbdd44f2490c313
Sagar <sagarmaurya814@gmail>
2023-08-09 13:30:52 +0530

    delete and edit functionality

--- index.js
+++ index.js
@@ -1,81 +1,43 @@
-// const header = document.querySelector("#main-header");
-// const title = document.querySelector(".title");
-// const lists = document.getElementsByClassName("list-group-item");
-// const listEl = document.getElementsByTagName("li");
-// console.log(listEl);
+const form = document.getElementById('addForm');
+const items = document.getElementById('items');
+
+form.addEventListener('submit',addItem);
+items.addEventListener('click',deleteItem);
 
 
-// title.style.color="green";
-// title.style.fontWeight="bold"
-// header.style.borderBottom ="5px solid black";
+// Add item to the list
 
-// // get elements by classname assignment
-// lists[2].style.backgroundColor="green";
-// for(let list of lists){
-//     list.style.fontWeight= "bold"
-// }
+function addItem(e){
+    e.preventDefault();
+    const item = document.getElementById('item').value;
+    const li = document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(item));
 
-
-// // get elements by tag name assignment
-// for(let list of lists){
-//     list.style.backgroundColor="#ccc"
-// }
-
-// for(let list of listEl){
-//     list.style.backgroundColor ="skyblue"
-// }
-
-
-// query and query all selector assingment
+    // delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    li.appendChild(deleteBtn)
+    
+    // edit button
+    const editBtn =document.createElement('button');
+    editBtn.className='btn btn-success btn-sm float-right mx-2';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(editBtn);
 
 
-// const list2 = document.querySelector("li:nth-child(2)");
-// const list3 = document.querySelector("li:nth-child(3)");
-
-// list2.style.backgroundColor="green" ;
-// list3.style.display ="none"
-
-// const lists = document.querySelectorAll("li");
-// const oddLists = document.querySelectorAll("li:nth-child(odd)");
-
-// lists[1].style.color="green";
-
-// for(let list of oddLists){
-//     list.style.backgroundColor="green"
-// }
+    items.appendChild(li);
+    document.getElementById('item').value=""
+}
 
 
-// creating nodes and modifying dom assignment
-
-const items = document.querySelector("#items");
-
-items.parentElement.style.backgroundColor="#f4f4f4";
-
-items.lastElementChild.style.backgroundColor="#ccc"
-items.lastChild.backgroundColor="black"
-
-// items.firstElementChild.textContent="Hello";
+// Delete item form the list
 
 
-
-// create element
-
-const newDiv = document.createElement('div');
-newDiv.className="item"
-newDiv.id='item1'
-newDiv.setAttribute('title','hello world')
-
-const newText = document.createTextNode('Hello');
-newDiv.appendChild(newText)
-items.appendChild(newDiv)
-
-
-const container = document.querySelector('header .container');
-const title= document.querySelector('#header-title');
-container.insertBefore(newDiv,title)
-
-
-const item1= items.firstElementChild;
-const li = items.firstElementChild
-item1.insertBefore(newDiv,li.firstElementChild)
-
+function deleteItem(e){
+    if(e.target.classList.contains('delete')){
+        const li = e.target.parentElement;
+        items.removeChild(li)
+    }
+}
